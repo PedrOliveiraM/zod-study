71f192ae7d99e88bf0d72c352d98be118c3483b6
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 15:47:59 -0300

    fear: create schema email

--- src/components/EmailForm/EmailForm.schema.ts
+++ src/components/EmailForm/EmailForm.schema.ts
@@ -4,4 +4,6 @@ export const schema = z.object({
   // Validação do email deve:
   // - Ser uma string
   // - Ser um email válido
+  email: z.string({message: 'Invalid Type'}).email({message: 'Invalid Email'}),
 });
+ 
