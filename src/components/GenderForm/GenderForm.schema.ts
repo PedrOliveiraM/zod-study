4ea0457c24132fbce1274790fd2f0319f8087e99
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 17:25:08 -0300

    feat: create schema for Gender Forms

--- src/components/GenderForm/GenderForm.schema.ts
+++ src/components/GenderForm/GenderForm.schema.ts
@@ -4,4 +4,5 @@ export const schema = z.object({
   // Validação do gênero deve:
   // - Ser uma string
   // - Ser um dos valores válidos: 'male', 'female', 'other'
+  gender: z.enum(['male','Female', 'Other'],{message: 'This Gender Is Not valid'})
 });
