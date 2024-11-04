7ffaba525d98ef26aa5f7052aa8a2a74abc38933
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 14:53:21 -0300

    feat: create basic form schema

--- src/components/BasicForm/BasicForm.schema.ts
+++ src/components/BasicForm/BasicForm.schema.ts
@@ -4,4 +4,7 @@ export const schema = z.object({
   // Validação do nome deve:
   // - Ser uma string
   // - Ter no mínimo 2 caracteres
+
+  name: z.string({message: 'Invalid Type'}).min(2, `name must have at least 2 characters`),
+
 });
