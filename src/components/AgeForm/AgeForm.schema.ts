3a6dfa3853c6d907e0d7a2c16ae5f8294028dbc0
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 17:12:05 -0300

    feat: create age schema

--- src/components/AgeForm/AgeForm.schema.ts
+++ src/components/AgeForm/AgeForm.schema.ts
@@ -5,4 +5,7 @@ export const schema = z.object({
   // - Ser um número
   // - Ser maior ou igual a 18
   // - Ser menor ou igual a 99
+  age: z.number()
+  .min(18,{message: 'Must be greater than or equal to 18'})
+  .max(99,{message: 'Must be less than or equal to 99'})
 });
