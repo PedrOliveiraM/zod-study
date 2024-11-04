6c72c927d89a395d2a87eedbed2e19bd67ffafef
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 17:34:41 -0300

    feat: create agreement Form with boolean and refine zod

--- src/components/AgreementForm/AgreementForm.schema.ts
+++ src/components/AgreementForm/AgreementForm.schema.ts
@@ -4,4 +4,5 @@ export const schema = z.object({
   // Validação do termo deve:
   // - Ser um booleano
   // - Ser true
-});
+  agreeToTerms: z.boolean().refine((value) => value === true, {message: 'Ow this field is necessary'})
+})
