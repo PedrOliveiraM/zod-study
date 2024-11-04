cf5b4cb86a458d1670f15a76ef55a56dd6897d88
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-04 18:11:15 -0300

    feat: create schema phone using regex zod

--- src/components/PhoneForm/PhoneForm.schema.ts
+++ src/components/PhoneForm/PhoneForm.schema.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const schema = z.object({
-  // Validação do telefone deve:
-  // - Ser uma string
-  // - Ter exatamente 10 dígitos
-});
+
+  phone: z.string().regex(/^\(\d{2}\)\d{5}-?\d{4}$/,{message: 'The phone field is poorly formatted, (xx) xxxxx-xxxx'})
+
+})
